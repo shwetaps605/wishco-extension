2244e23142a6d85d948933bc6ac70e75680a15a3
Roy <shweroy@apac.corpdir.net>
2024-06-28 14:47:19 +0530

    setup working

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="p-5">
+      <h1 className='text-2xl text-primary'>
+        wishlist your company at 
+      </h1>
+    </div>
+  );
+}
+
+export default App;
